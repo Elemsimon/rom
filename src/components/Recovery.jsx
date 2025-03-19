eed3ee487e31690386d137f6f0370e7f5a49ed8e
Elemsimon <elemifesinachisimon@gmail.com>
2025-03-19 20:46:22 +0100

    update

--- src/components/Recovery.jsx
+++ src/components/Recovery.jsx
@@ -1,3 +1,4 @@
+import Button from './Button'
 import recoveryImage from '/assets/recovery.png'
 
 const Recovery = () => {
@@ -7,10 +8,11 @@ const Recovery = () => {
         <div className='recoveryRectangle'>
           <h4 className='recoveryText'>Recovery</h4>
           <h4 className='recoveryText' style={{WebkitTextFillColor:'#ffffff'}}>Recovery</h4>
-          <h4 className='recoveryText'>Recovery</h4>
+          <h4 className='recoveryText' style={{paddingBottom:'20px'}}>Recovery</h4>
+          <Button title='Buy Now'/>
         </div>
         <div>
-          <img src={recoveryImage} alt="recovery-image" style={{objectFit:'cover'}}/>
+          <img src={recoveryImage} alt="recovery-image" className="recovery-image"/>
         </div>
       </div>
     </div>
--- src/components/WhyChoose.jsx
+++ src/components/WhyChoose.jsx
@@ -5,11 +5,11 @@ import GetData from './GetData'
 const WhyChoose = () => {
   return (
     <div className='container'>
-      <div className='d-flex justify-content-between '>
+      <div className='d-flex justify-content-between whychoose'>
         <div className='bg'>
-          <img src={Gun} alt="Image" className=''/>
+          <img src={Gun} alt="Image" className='img'/>
         </div>
-        <div className='d-flex flex-column col-6'>
+        <div className='d-flex flex-column col-lg-6'>
           <h1 className='header3 fs-1 w-100 mb-4'>What makes this gun different from the rest</h1>
           <div className='d-flex justify-content-end gap-3'>
             <div className='w-50'>
