b2ad8b7d5378496fa0f1769eadcc7ef6354e2991
Elemsimon <elemifesinachisimon@gmail.com>
2025-03-25 19:01:32 +0100

    update

--- src/components/Recovery.jsx
+++ src/components/Recovery.jsx
@@ -1,15 +1,13 @@
-import Button from './Button'
 import recoveryImage from '/assets/recovery.png'
 
 const Recovery = () => {
   return (
-    <div className='recoveryContainer'>
+    <div className='recoveryContainer' data-aos="fade" data-aos-delay="700">
       <div className='recovery'>
         <div className='recoveryRectangle'>
           <h4 className='recoveryText'>Recovery</h4>
           <h4 className='recoveryText' style={{WebkitTextFillColor:'#ffffff'}}>Recovery</h4>
           <h4 className='recoveryText' style={{paddingBottom:'20px'}}>Recovery</h4>
-          <Button title='Buy Now'/>
         </div>
         <div>
           <img src={recoveryImage} alt="recovery-image" className="recovery-image"/>
--- src/components/SmartHit.jsx
+++ src/components/SmartHit.jsx
@@ -2,7 +2,7 @@ import Smart from '/assets/smartHit.png'
 
 const SmartHit = () => {
   return (
-    <div>
+    <div data-aos="fade" data-aos-delay="700">
       <img src={Smart} alt="Smart-Hit" className='smartHit'/>
     </div>
   )
